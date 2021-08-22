fabf5d1641c7cd5f6a2bb00d75ded0130cfa7ec2
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2021-08-23 00:11:19 +0200

    release first version

---
+++ asptt_disabled_admin_login_form_login_head.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package ASPTT_Disable_Admin_Login_Form
+ * @version 1.0
+ */
+/*
+Plugin Name: ASPTT - Disable Admin Login Form
+Plugin URI: https://www.badaspttstrasbourg.fr
+Description: Hide username and password fields from Admin Login form.
+Author: Loïc Leuilliot
+Version: 1.0
+Author URI: https://github.com/organizations/ASPTT-Strasbourg
+*/
+
+add_action( 'login_head', 'asptt_disable_admin_login_form_login_head' );
+
+function asptt_disable_admin_login_form_login_head() {
+    $style = '<style type="text/css">';
+    $style .= '#loginform > p { display: none; }';
+    $style .= '.user-pass-wrap { display: none; }';
+    $style .= '</style>';
+    
+    echo $style;
+}
